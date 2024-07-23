e50d5f1f6d441fbeeb13511f2144bccef61bf613
Lucky Rathore <luckyrathore03@gmail.com>
2024-07-23 19:41:40 +0530

    publish script

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/newsappp.github.io",
+    basePath: "/",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
