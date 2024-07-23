2d662bf07da809a86964b2267c352146dbf369dc
Lucky Rathore <luckyrathore03@gmail.com>
2024-07-23 22:06:08 +0530

    react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import NewsList from './NewsList';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
