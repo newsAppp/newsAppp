142a157ac9346bd2b85afc31f20c3d1f7fad5112
Lucky Rathore <luckyrathore03@gmail.com>
2024-08-10 13:59:15 +0530

    branding v1

--- src/components/AppAppBar.js
+++ src/components/AppAppBar.js
@@ -118,9 +118,7 @@ function AppAppBar({ mode, toggleColorMode, handleCategoryChange }) {
               }}
             >
               <img
-                src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
-                }
+                src={'/logo/vector/default.svg' }
                 style={logoStyle}
                 alt="logo of sitemark"
               />
