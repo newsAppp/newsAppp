94df6dbd5e5d1000eee03cdfe4ffeef435d37a45
Lucky Rathore <luckyrathore03@gmail.com>
2024-08-21 18:22:09 +0530

    mobile v1 running

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.luckyrathore.newsflash',
+  appName: 'News Flash',
+  webDir: 'build'
+};
+
+export default config;
